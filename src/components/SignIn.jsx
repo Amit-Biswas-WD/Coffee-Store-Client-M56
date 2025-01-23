722aec03eddea26754d486e90c5475de4de5c5bf
Amit biswas <amitbiswas9282@gmail.com>
2025-01-23 16:27:11 +0600

    Sign In

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaEyeSlash } from "react-icons/fa";
+import { IoIosEye } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
-  return <div>Login</div>;
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSignInForm = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const value = { email, password };
+    console.log(value);
+  };
+
+  return (
+    <div className="container mx-auto">
+      <h2 className="text-2xl font-bold">SignUp Now!</h2>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="card bg-base-100 w-full max-w-xl my-8 shrink-0 shadow-2xl">
+          <form onSubmit={handleSignInForm} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type={showPassword ? "password" : "text"}
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <p
+                onClick={() => setShowPassword(!showPassword)}
+                className="btn btn-xs absolute mt-12 right-12"
+              >
+                {showPassword ? <FaEyeSlash /> : <IoIosEye />}
+              </p>
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Sign Up</button>
+            </div>
+            <Link className="text-end my-2" to={`/login`}>
+              Click Here to <span className="text-blue-900">Login</span>
+            </Link>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
