3e35ede1276500ad5e33ec5bd6292bb5366562f3
Amit biswas <amitbiswas9282@gmail.com>
2025-01-23 16:22:39 +0600

    SignUp

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,9 +1,105 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaEyeSlash } from "react-icons/fa";
+import { IoIosEye } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+  const [showPassword, setShowPassword] = useState(false);
 
-export default SignUp
+  const handleSignUpForm = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    const value = { name, email, password };
+    console.log(value);
+  };
+
+  return (
+    <div className="container mx-auto">
+      <h2 className="text-2xl font-bold">SignUp Now!</h2>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="card bg-base-100 w-full max-w-xl my-8 shrink-0 shadow-2xl">
+          <form onSubmit={handleSignUpForm} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="name"
+                name="name"
+                placeholder="name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Photo Url</span>
+              </label>
+              <input
+                type="text"
+                name="photo"
+                placeholder="Photo Url"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type={showPassword ? "password" : "text"}
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <p
+                onClick={() => setShowPassword(!showPassword)}
+                className="btn btn-xs absolute mt-12 right-12"
+              >
+                {showPassword ? <FaEyeSlash /> : <IoIosEye />}
+              </p>
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control">
+              <label className="label cursor-pointer">
+                <input type="checkbox" name="trams" className="checkbox" />
+                <span className="label-text">
+                  Accepts Our Teams And Conditions
+                </span>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Sign Up</button>
+            </div>
+            <Link className="text-end my-2" to={`/login`}>
+              Click Here to <span className="text-blue-900">Login</span>
+            </Link>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,25 +3,44 @@ import App from "../App";
 import AddCoffee from "../components/AddCoffee";
 import UpdateCoffee from "../components/UpdateCoffee";
 import CoffeeDetails from "../components/CoffeeDetails";
+import SignUp from "../components/SignUp";
+import SignIn from "./../components/SignIn";
+import Main from "../Main/Main";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-    loader: () => fetch("http://localhost:5000/coffee"),
-  },
-  {
-    path: "/addCoffee",
-    element: <AddCoffee />,
-  },
-  {
-    path: "/updateCoffee/:id",
-    element: <UpdateCoffee />,
-    loader: ({ params }) => fetch(`http://localhost:5000/coffee/${params.id}`),
-  },
-  {
-    path: "/coffeeDetails/:id",
-    element: <CoffeeDetails />,
-    loader: ({ params }) => fetch(`http://localhost:5000/coffee/${params.id}`),
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+        loader: () => fetch("http://localhost:5000/coffee"),
+      },
+      {
+        path: "/addCoffee",
+        element: <AddCoffee />,
+      },
+      {
+        path: "/updateCoffee/:id",
+        element: <UpdateCoffee />,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/coffee/${params.id}`),
+      },
+      {
+        path: "/coffeeDetails/:id",
+        element: <CoffeeDetails />,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/coffee/${params.id}`),
+      },
+      {
+        path: "/sign-up",
+        element: <SignUp />,
+      },
+      {
+        path: "/sign-in",
+        element: <SignIn />,
+      },
+    ],
   },
 ]);
